26cfe1ef95f708728597fbe9a34c3683c433c80d
abdelrhman-abdu-ali <60532455+abdelrhman-abdu-ali@users.noreply.github.com>
2024-07-21 14:11:21 +0300

    21/7

--- Source/ThirdGameShooterCpp/ShooterCharacter.h
+++ Source/ThirdGameShooterCpp/ShooterCharacter.h
@@ -4,16 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AmmoType.h"
 #include "ShooterCharacter.generated.h"
 
-UENUM(BlueprintType)
-enum class EAmmoType : uint8
-{
-	EAT_9mm UMETA(DisplayName = "9mm"),
-	EAT_AR UMETA(DisplayName = "AssaultRifle"),
-
-	EAT_NAX UMETA(DisplayName = "DefaultMAX")
-};
 
 UENUM(BlueprintType)
 enum class ECombatState : uint8
