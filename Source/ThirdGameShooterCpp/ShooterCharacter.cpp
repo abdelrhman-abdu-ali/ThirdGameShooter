0238cd9ba4a2400d163e948e494507858d8a1ecd
abdelrhman-abdu-ali <60532455+abdelrhman-abdu-ali@users.noreply.github.com>
2024-08-11 04:58:52 +0300

    11/82

--- Source/ThirdGameShooterCpp/ShooterCharacter.cpp
+++ Source/ThirdGameShooterCpp/ShooterCharacter.cpp
@@ -143,6 +143,7 @@ void AShooterCharacter::BeginPlay()
 	}
 	// Spawn the default weapon and equip it
 	EquipWeapon(SpawnDefaultWeapon());
+	Inventory.Add(EquippedWeapon);
 	EquippedWeapon->DisableCustomDepth();
 	EquippedWeapon->DisableGlowMaterial();
 
@@ -1021,7 +1022,14 @@ void AShooterCharacter::GetPickupItem(AItem* Item)
 	auto Weapon = Cast<AWeapon>(Item);
 	if (Weapon)
 	{
-		SwapWeapon(Weapon);
+		if (Inventory.Num() < INVENTORY_CAPACITY)
+		{
+			Inventory.Add(Weapon);
+		}
+		else // Inventory is full! Swap with EquippedWeapon
+		{
+			SwapWeapon(Weapon);
+		}
 	}
 
 	auto Ammo = Cast<AAmmo>(Item);
